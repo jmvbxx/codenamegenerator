b4318a6db965523a1d5f44fe93ac87cabbb9b6e1
Jason Bornhoft <jmvbxx@gmail.com>
2023-01-19 14:35:21 -0500

    Drop code into place

---
+++ namegenerator.go
@@ -0,0 +1,49 @@
+package gonamegenerator
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func NameGenerate() {
+	s := rand.NewSource(time.Now().UTC().UnixNano())
+	r := rand.New(s)
+
+	r1 := CommonCodeNames[r.Intn(len(CommonCodeNames))]
+	r2 := CommonCodeNames[r.Intn(len(CommonCodeNames))]
+	r3 := CommonCodeNames[r.Intn(len(CommonCodeNames))]
+
+	resp, err := http.PostForm("https://www.codenamegenerator.com/",
+		url.Values{"prefix": {r1}, "dictionary": {r2}, "suffix": {r3}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var words []string
+
+	doc.Find("b:first-child").Each(func(i int, s *goquery.Selection) {
+		words = append(words, s.Text())
+	})
+
+	l := strings.ToLower(words[0])
+
+	split := strings.Split(l, "\n")
+
+	fmt.Printf("%s-%s-%s", split[0], split[1], split[2])
+}
---
+++ vars.go
@@ -0,0 +1,7 @@
+package gonamegenerator
+
+var (
+	CommonCodeNames = []string{"animals", "apple", "birds", "bugs", "dogs", "fishes",
+		"flowers", "metal", "microsoft", "snakes", "units", "unusual_animals", "us_cities",
+		"us_counties", "us_state_capitals", "us_states", "world_capitals", "world_countries"}
+)
